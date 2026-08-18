d67d281d84d160e0d58d24e595625c91f6b074e0
Ida Bagus Adimas <aridirat@gmail.com>
2026-08-18 09:30:18 +0800

    memperbaiki command dan controller untuk memanggil produsen_id dari rujukan

--- app/Console/Commands/FillDataProdusenFromMetadata.php
+++ app/Console/Commands/FillDataProdusenFromMetadata.php
@@ -5,18 +5,19 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class FillDataProdusenFromMetadata extends Command
+class FillDataProdusenFromRujukan extends Command
 {
     protected $signature = 'data:fill-produsen {--force : Overwrite existing produsen_id in data}';
 
-    protected $description = 'Isi kolom produsen_id pada tabel data berdasarkan produsen_id dari metadata terkait';
+    protected $description = 'Isi kolom produsen_id pada tabel data berdasarkan produsen_id dari rujukan terkait';
 
     public function handle(): int
     {
         $force = (bool) $this->option('force');
 
         $countQuery = DB::table('data')
-            ->join('metadata', 'data.metadata_id', '=', 'metadata.metadata_id');
+            ->join('rujukan', 'data.rujukan_id', '=', 'rujukan.rujukan_id')
+            ->whereNotNull('rujukan.produsen_id');
 
         if (!$force) {
             $countQuery->whereNull('data.produsen_id');
@@ -35,9 +36,9 @@ class FillDataProdusenFromMetadata extends Command
         }
 
         if ($force) {
-            DB::statement('UPDATE `data` JOIN `metadata` ON `data`.`metadata_id` = `metadata`.`metadata_id` SET `data`.`produsen_id` = `metadata`.`produsen_id`');
+            DB::statement('UPDATE `data` JOIN `rujukan` ON `data`.`rujukan_id` = `rujukan`.`rujukan_id` SET `data`.`produsen_id` = `rujukan`.`produsen_id` WHERE `rujukan`.`produsen_id` IS NOT NULL');
         } else {
-            DB::statement('UPDATE `data` JOIN `metadata` ON `data`.`metadata_id` = `metadata`.`metadata_id` SET `data`.`produsen_id` = `metadata`.`produsen_id` WHERE `data`.`produsen_id` IS NULL');
+            DB::statement('UPDATE `data` JOIN `rujukan` ON `data`.`rujukan_id` = `rujukan`.`rujukan_id` SET `data`.`produsen_id` = `rujukan`.`produsen_id` WHERE `data`.`produsen_id` IS NULL AND `rujukan`.`produsen_id` IS NOT NULL');
         }
 
         $this->info("Selesai. {$count} baris di-update.");
